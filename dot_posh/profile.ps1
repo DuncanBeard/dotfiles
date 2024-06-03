1e916953097218789fb67e59495b9d55a339bb2a
Duncan Beard <duncanbeard@microsoft.com>
2024-06-03 07:49:07 -0400

    Update PowerShell profile with oh-my-posh initialization and aliases

--- dot_posh/profile.ps1
+++ dot_posh/profile.ps1
@@ -1,24 +1,15 @@
-New-Alias which Get-Command
-New-Alias touch New-Item
-New-Alias cz chezmoi
-New-Alias reload ". $HOME\.posh\profile.ps1"
-New-Alias ".." "Set-Location .."
-New-Alias ... "Set-Location ..\.."
-New-Alias .... "Set-Location ..\..\.."
+oh-my-posh init pwsh | Invoke-Expression
 
-$GH_COPILOT_PROFILE = Join-Path -Path $(Split-Path -Path $PROFILE -Parent) -ChildPath "gh-copilot.ps1"
-gh copilot alias -- pwsh | Out-File ( New-Item -Path $GH_COPILOT_PROFILE -Force )
-echo ". `"$GH_COPILOT_PROFILE`"" >> $PROFILE
+. "C:\Users\duncanbeard\.posh\gh-copilot.ps1"
+
+Set-Alias which Get-Command
+Set-Alias touch New-Item
+Set-Alias cz chezmoi
 
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
 # Set-PSReadlineKeyHandler -Key Tab -Function AcceptSuggestion
-# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # oh-my-posh init pwsh --config "$HOME\.atomic.omp.json" | Invoke-Expression
 # oh-my-posh init pwsh --config "$HOME\.atomic-left-only.omp.json" | Invoke-Expression
-oh-my-posh init pwsh | Invoke-Expression
-
-$env:Path += "C:\src\scripts"
