18d5db1819ac9108d45efd3c365aa50931efbee6
Duncan Beard <duncanbeard@microsoft.com>
2025-09-03 08:28:20 -0400

    Remove Copilot CLI integration

--- dot_powershell/profile.ps1
+++ dot_powershell/profile.ps1
@@ -14,11 +14,4 @@ Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 # Set-PSReadlineKeyHandler -Key Tab -Function AcceptSuggestion
-
-Set-PSReadLineOption -HistorySavePath 'C:\Users\duncanbeard\OneDrive - Microsoft\Documents\PowerShell\history.txt'
-
-#GitHub CoPilot CLI
-$ghcopilotpath = "$HOME\gh-copilot.ps1"
-if (Test-Path $ghcopilotpath) {
-  . $ghcopilotpath
-}
+Set-PSReadLineOption -HistorySavePath 'C:\Users\duncanbeard\OneDrive - Microsoft\PowerShell\history.txt'
